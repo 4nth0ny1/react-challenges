235daee5554292342dd7cbbda686575c4c048707
Anthony C <44069058+4nth0ny1@users.noreply.github.com>
2023-05-31 18:36:04 -0500

    submit form (#1)
    Co-authored-by: Anthony Catullo <anthonycatullo@oatslam.local>

--- app/page.js
+++ app/page.js
@@ -4,7 +4,8 @@ export default function Home() {
   return (
     <>
       <h2>React Challenges</h2>
-      <Link href="/progressbar">ProgressBar</Link>
+      <Link href="/progressbar">Progress Bar</Link>
+      <Link href="/submitformdata">Submit Form Data</Link>
     </>
   );
 }
